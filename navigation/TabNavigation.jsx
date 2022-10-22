2eb8d6b9e7c43a8b26817bdaa106fade3fff9bcc
codewithkevin <oseikelvin709@gmail.com>
2022-10-22 13:33:05 +0000

    Removed Tab bar width

--- navigation/TabNavigation.jsx
+++ navigation/TabNavigation.jsx
@@ -3,31 +3,46 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../Screens/HomeScreen";
 import ProfileScreen from "../Screens/ProfileScreen";
 import CategoryScreen from "./../Screens/CategoryScreen";
-import NotificationScreen from './../Screens/NotificationScreen';
+import NotificationScreen from "./../Screens/NotificationScreen";
 
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          borderTopWidth: 0,
+        },
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{ tabBarIcon: makeIconRender("home-outline"), headerShown: false }}
-      />
-      <Tab.Screen
-        name="Settings"
-        component={ProfileScreen}
-        options={{ tabBarIcon: makeIconRender("cog"), headerShown: false }}
-      />
-      <Tab.Screen
-        name="Message"
-        component={NotificationScreen}
-        options={{ tabBarIcon: makeIconRender("cog"), headerShown: false }}
+        options={{
+          tabBarIcon: makeIconRender("home-outline"),
+          headerShown: false,
+        }}
       />
       <Tab.Screen
         name="Category"
         component={CategoryScreen}
+        options={{
+          tabBarIcon: makeIconRender("dots-grid"),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Message"
+        component={NotificationScreen}
+        options={{
+          tabBarIcon: makeIconRender("message-badge-outline"),
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={ProfileScreen}
         options={{ tabBarIcon: makeIconRender("cog"), headerShown: false }}
       />
     </Tab.Navigator>
