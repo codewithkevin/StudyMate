2488406c7e564bd39bffa261f95ac2570b8cd531
codewithkevin <oseikelvin709@gmail.com>
2022-10-22 10:02:28 +0000

    Created Sperate Folder for Tab Navigation

--- App.js
+++ App.js
@@ -1,33 +1,10 @@
-import { Text, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import HomeScreen from "./Screens/HomeScreen";
-import ProfileScreen from "./Screens/ProfileScreen";
-
-const Tab = createBottomTabNavigator();
+import TabNavigation from "./navigation/TabNavigation";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ tabBarIcon: makeIconRender("home"), headerShown: false }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={ProfileScreen}
-          options={{ tabBarIcon: makeIconRender("cog"), headerShown: false }}
-        />
-      </Tab.Navigator>
+      <TabNavigation />
     </NavigationContainer>
-  );
-}
-
-function makeIconRender(name) {
-  return ({ color, size }) => (
-    <MaterialCommunityIcons name={name} color={color} size={size} />
   );
 }
