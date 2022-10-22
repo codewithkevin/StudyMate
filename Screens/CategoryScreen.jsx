29dd3f1f5b217896c6132afcf4726f88a0f7d810
codewithkevin <oseikelvin709@gmail.com>
2022-10-22 13:08:57 +0000

    Created All Screens For applications

---
+++ Screens/CategoryScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const CategoryScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>CategoryScreen</Text>
+    </View>
+  );
+}
+
+export default CategoryScreen
---
+++ Screens/NotificationScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const NotificationScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>NotificationScreen</Text>
+    </View>
+  );
+}
+
+export default NotificationScreen
