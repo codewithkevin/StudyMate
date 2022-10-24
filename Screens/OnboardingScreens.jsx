219085e898c94258da2e795affdc6b02213c17c4
codewithkevin <oseikelvin709@gmail.com>
2022-10-24 09:26:32 +0000

    Created OnBoarding Screen

--- Screens/OnboardingScreens.jsx
+++ Screens/OnboardingScreens.jsx
@@ -1,12 +1,94 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from "react-native";
 
-const OnboardingScreens = () => {
+import Onboarding from "react-native-onboarding-swiper";
+
+const Dots = ({ selected }) => {
+  let backgroundColor;
+
+  backgroundColor = selected ? "rgba(0, 0, 0, 0.8)" : "rgba(0, 0, 0, 0.3)";
+
   return (
-    <View>
-      <Text>OnboardingScreens</Text>
-    </View>
-  )
-}
+    <View
+      style={{
+        width: 6,
+        height: 6,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
+  );
+};
 
-export default OnboardingScreens
+const Skip = ({ ...props }) => (
+  <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+    <Text style={{ fontSize: 16 }}>Skip</Text>
+  </TouchableOpacity>
+);
+
+const Next = ({ ...props }) => (
+  <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+    <Text style={{ fontSize: 16 }}>Next</Text>
+  </TouchableOpacity>
+);
+
+const Done = ({ ...props }) => (
+  <TouchableOpacity style={{ marginHorizontal: 10 }} {...props}>
+    <Text style={{ fontSize: 16 }}>Done</Text>
+  </TouchableOpacity>
+);
+
+const OnboardingScreens = ({ navigation }) => {
+  return (
+    <Onboarding
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      DotComponent={Dots}
+      onSkip={() => navigation.replace("Login")}
+      onDone={() => navigation.navigate("Login")}
+      pages={[
+        {
+          backgroundColor: "#a6e4d0",
+          image: (
+            <Image
+              source={require("../Assest/3d-business-man-and-woman-working-at-the-table.png")}
+            />
+          ),
+          title: "Connect to the World",
+          subtitle: "A New Way To Connect With The World",
+        },
+        {
+          backgroundColor: "#fdeb93",
+          image: (
+            <Image
+              source={require("../Assest/3d-business-man-and-woman-working-at-the-table.png")}
+            />
+          ),
+          title: "Share Your Favorites",
+          subtitle: "Share Your Thoughts With Similar Kind of People",
+        },
+        {
+          backgroundColor: "#e9bcbe",
+          image: (
+            <Image
+              source={require("../Assest/3d-business-man-and-woman-working-at-the-table.png")}
+            />
+          ),
+          title: "Become The Star",
+          subtitle: "Let The Spot Light Capture You",
+        },
+      ]}
+    />
+  );
+};
+
+export default OnboardingScreens;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
