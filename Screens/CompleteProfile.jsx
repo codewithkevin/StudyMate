cdcfe817b7675b0bb95dd783208ebe9656271c7b
codewithkevin <oseikelvin709@gmail.com>
2022-10-25 23:53:04 +0000

    Update

--- Screens/CompleteProfile.jsx
+++ Screens/CompleteProfile.jsx
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   Button,
   Image,
+  StyleSheet,
 } from "react-native";
 import React, { useState } from "react";
 import { useRoute } from "@react-navigation/native";
@@ -13,12 +14,31 @@ import { collection, addDoc } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { AntDesign } from "@expo/vector-icons";
 import { db } from "../firebase";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import * as ImagePicker from "expo-image-picker";
 
 const CompleteProfile = () => {
   const [validationMessage, setValidationMessage] = useState("");
   const [gender, setGender] = useState("");
+  const [username, setUsername] = useState("");
   const [image, setImage] = useState(null);
+
+  const [datePicker, setDatePicker] = useState(false);
+
+  const [date, setDate] = useState(new Date());
+
+  const [timePicker, setTimePicker] = useState(false);
+
+  const [time, setTime] = useState(new Date(Date.now()));
+
+  function showDatePicker() {
+    setDatePicker(true);
+  }
+
+  function onDateSelected(event, value) {
+    setDate(value);
+    setDatePicker(false);
+  }
 
   //ROutes
   const route = useRoute();
@@ -67,8 +87,15 @@ const CompleteProfile = () => {
 
   return (
     <View className="p-5 mt-10">
-      <View>
-        <Text className="mb-5 text-center text-2xl">Complete Profile</Text>
+      <View className="flex flex-row space-x-1">
+        <View className="mt-1">
+          <TouchableOpacity>
+            <AntDesign name="arrowleft" size={28} color="black" />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <Text className="mb-5 text- text-2xl">Fill your Profile</Text>
+        </View>
       </View>
       <View className="flex justify-center items-center static">
         {image ? (
@@ -95,8 +122,70 @@ const CompleteProfile = () => {
           </TouchableOpacity>
         </View>
       </View>
+      <View>
+        <View className="mb-7 mt-4">
+          <TextInput
+            className="bg-gray-200 border border-gray-400 text-black text-sm rounded-[10px] block w-full p-4 placeholder-black"
+            placeholder="UserName"
+            placeholderTextColor="#000"
+            containerStyle={{ marginTop: 10, backgroundColor: "white" }}
+            value={username}
+            onChangeText={(text) => setUsername(text)}
+          />
+        </View>
+
+        <View>
+          <View className="relative">
+            <TextInput
+              className="bg-gray-200 border border-gray-400 text-black text-sm rounded-[10px] block w-full p-4 placeholder-black"
+              placeholder={date.toDateString()}
+              placeholderTextColor="#000"
+              containerStyle={{ marginTop: 10, backgroundColor: "white" }}
+              required
+            />
+            <TouchableOpacity onPress={showDatePicker}>
+              <Text className="text-white absolute right-2.5 bottom-2.5   focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-4 py-2">
+                <AntDesign name="camerao" size={24} color="black" />
+              </Text>
+            </TouchableOpacity>
+            {datePicker && (
+              <DateTimePicker
+                value={date}
+                mode={"date"}
+                display={Platform.OS === "ios" ? "spinner" : "default"}
+                is24Hour={true}
+                onChange={onDateSelected}
+                style={styleSheet.datePicker}
+              />
+            )}
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
 
 export default CompleteProfile;
+
+const styleSheet = StyleSheet.create({
+  text: {
+    fontSize: 25,
+    color: "red",
+    padding: 3,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+
+  // Style for iOS ONLY...
+  datePicker: {
+    justifyContent: "right",
+    alignItems: "flex-start",
+    borderRadius: "52px",
+    marginTop: 10,
+    width: 320,
+    height: 260,
+    display: "flex",
+    backgroundColor: "gray",
+    color: "gray",
+  },
+});
