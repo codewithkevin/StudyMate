8e652639ab2fda181053e9cb8ed542c678a0e7d4
codewithkevin <oseikelvin709@gmail.com>
2022-10-25 08:27:36 +0000

    Update Tab

--- navigation/index.jsx
+++ navigation/index.jsx
@@ -1,6 +1,6 @@
 import AuthStack from "./AuthStack";
-import { useAuthentication } from './../Hooks/useAuthentication';
-import MyStack from './MyStack';
+import { useAuthentication } from "./../Hooks/useAuthentication";
+import MyStack from "./MyStack";
 
 export default function RootNavigation() {
   const { user } = useAuthentication();
