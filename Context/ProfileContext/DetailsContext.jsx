40d3bf56d3f7423fd0d442e7d282d43e70b58015
codewithkevin <oseikelvin709@gmail.com>
2022-10-29 09:43:32 +0000

    Update Prev Changes

--- Context/ProfileContext/DetailsContext.jsx
+++ Context/ProfileContext/DetailsContext.jsx
@@ -4,11 +4,71 @@ import React, { useState, useEffect } from "react";
 export const DetailsContext = React.createContext();
 
 const DetailsProvider = (props) => {
+  //Purpose State
   const [purpose, setPurpose] = useState("");
   const [selectedPurpose, setSelectedPurpose] = useState(true);
   const [studyselected, setStudySelected] = useState(true);
 
-  function fiendshippurpose() {
+  //Gender Context
+  const [gender, setGender] = useState("");
+  const [male, setMale] = useState(true);
+  const [female, setFemale] = useState(true);
+  const [unisex, setUnisex] = useState(true);
+
+  //Occupation Context
+  const [occupation, setOccupation] = useState("");
+  const [student, setStudent] = useState(true);
+  const [occupant, setOccupant] = useState(true);
+
+  //Occupation function
+  function studentfuction() {
+    if (occupant == false) {
+      return null;
+    } else {
+      setStudent((current) => !current);
+      student ? setOccupation("Student") : setOccupation("");
+    }
+  }
+
+  function occupantfunction() {
+    if (student == false) {
+      return null;
+    } else {
+      setOccupant((current) => !current);
+      occupant ? setOccupation("Worker") : setOccupation("");
+    }
+  }
+
+  //Gender Functions
+  function malefunction() {
+    if (female == false || unisex == false) {
+      return null;
+    } else {
+      setMale((current) => !current);
+      male ? setGender("Male") : setGender("");
+    }
+  }
+
+  function femalefunction() {
+    if (male == false || unisex == false) {
+      return null;
+    } else {
+      setFemale((current) => !current);
+      female ? setGender("Female") : setGender("");
+    }
+  }
+
+  function unisexfunction() {
+    if (male == false || female == false) {
+      return null;
+    } else {
+      setUnisex((current) => !current);
+      unisex ? setGender("Unisex") : setGender("");
+    }
+  }
+
+  //Purpose Functions
+  function friendshipfunction() {
     if (studyselected == false) {
       return null;
     } else {
@@ -27,12 +87,33 @@ const DetailsProvider = (props) => {
   }
 
   const values = {
+    occupantfunction,
+    studentfuction,
+    occupant,
+    setOccupant,
+    student,
+    setStudent,
+    occupation,
+    setOccupation,
+    unisex,
+    setUnisex,
+    unisexfunction,
+    female,
+    setFemale,
+    femalefunction,
+    malefunction,
+    gender,
+    setGender,
+    male,
+    setMale,
     purpose,
     setPurpose,
     selectedPurpose,
     setSelectedPurpose,
     studyselected,
     setStudySelected,
+    friendshipfunction,
+    studyfunctions,
   };
   return (
     <DetailsContext.Provider value={values}>
--- Screens/ProfileSetup/CompleteProfile.jsx
+++ Screens/ProfileSetup/CompleteProfile.jsx
@@ -7,26 +7,57 @@ import {
   Image,
   StyleSheet,
 } from "react-native";
-import React, { useState } from "react";
-import { useRoute } from "@react-navigation/native";
-import { useNavigation } from "@react-navigation/native";
+import React, { useState, useContext } from "react";
+import { useNavigation, useRoute } from "@react-navigation/native";
+
+//Context Import
+import { DetailsContext } from "./../../Context/ProfileContext/DetailsContext";
+
+//Firebase Support
 import { collection, addDoc } from "firebase/firestore";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { AntDesign } from "@expo/vector-icons";
+import { db } from "./../../firebase";
+
+//Icons Support
 import { Ionicons } from "@expo/vector-icons";
 import { Foundation } from "@expo/vector-icons";
 import { FontAwesome5 } from "@expo/vector-icons";
-
+import { AntDesign } from "@expo/vector-icons";
 import { Entypo } from "@expo/vector-icons";
-import { db } from "./../../firebase";
 
 const CompleteProfile = () => {
   //ROutes
   const route = useRoute();
 
-  const [purpose, setPurpose] = useState("");
-  const [selectedPurpose, setSelectedPurpose] = useState(true);
-  const [studyselected, setStudySelected] = useState(true);
+  const {
+    occupantfunction,
+    studentfuction,
+    occupant,
+    setOccupant,
+    student,
+    setStudent,
+    occupation,
+    setOccupation,
+    unisex,
+    setUnisex,
+    unisexfunction,
+    female,
+    setFemale,
+    femalefunction,
+    malefunction,
+    gender,
+    setGender,
+    male,
+    setMale,
+    purpose,
+    setPurpose,
+    selectedPurpose,
+    setSelectedPurpose,
+    studyselected,
+    setStudySelected,
+    friendshipfunction,
+    studyfunctions,
+  } = useContext(DetailsContext);
 
   //UseRoute is deprecated in favor of useNavigation
   const email = route.params.email;
@@ -42,9 +73,9 @@ const CompleteProfile = () => {
         email,
         password,
         name,
-        date,
-        completed,
-        username,
+        purpose,
+        gender,
+        occupation,
       })
         .then((response) => {
           console.log(response);
@@ -58,26 +89,6 @@ const CompleteProfile = () => {
     }
   }
 
-  function fiendshippurpose() {
-    if (studyselected == false) {
-      return null;
-    } else {
-      setSelectedPurpose((current) => !current);
-      selectedPurpose ? setPurpose("friends") : setPurpose("");
-    }
-  }
-
-  function studyfunctions() {
-    if (selectedPurpose == false) {
-      return null;
-    } else {
-      setStudySelected((current) => !current);
-      studyselected ? setPurpose("Study Mate") : setPurpose("");
-    }
-  }
-
-  console.log(purpose);
-
   return (
     <View className="p-5 mt-10">
       <View className="mb-5">
@@ -85,7 +96,7 @@ const CompleteProfile = () => {
         <View className="flex flex-row justify-evenly mb-5">
           <View className="mt-5">
             <TouchableOpacity
-              onPress={fiendshippurpose}
+              onPress={friendshipfunction}
               className={`border ${
                 selectedPurpose ? "border-slate-400" : "bg-[#075ADE]"
               } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
@@ -107,7 +118,11 @@ const CompleteProfile = () => {
                 studyselected ? "border-slate-400" : "bg-[#075ADE]"
               } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
             >
-              <AntDesign name="book" size={24} color="black" />
+              <AntDesign
+                name="book"
+                size={24}
+                color={`${studyselected ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Study Mate</Text>
@@ -116,62 +131,107 @@ const CompleteProfile = () => {
         </View>
       </View>
 
-      {/* <View className="mb-5">
+      <View className="mb-5">
         <Text className="text-2xl">Your Gender</Text>
         <View className="flex flex-row justify-evenly mb-5">
           <View className="mt-5">
-            <TouchableOpacity className="border border-slate-400 p-5 w-[84] h-[84] justify-center rounded-full items-center">
-              <Foundation name="male" size={24} color="black" />
+            <TouchableOpacity
+              onPress={malefunction}
+              className={`border ${
+                male ? "border-slate-400" : "bg-[#075ADE]"
+              } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
+            >
+              <Foundation
+                name="male"
+                size={24}
+                color={`${male ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Male</Text>
             </View>
           </View>
           <View className="mt-5">
-            <TouchableOpacity className="border border-slate-400 p-5 w-[84] h-[84] justify-center rounded-full items-center">
-              <FontAwesome5 name="female" size={24} color="black" />
+            <TouchableOpacity
+              onPress={femalefunction}
+              className={`border ${
+                female ? "border-slate-400" : "bg-[#075ADE]"
+              } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
+            >
+              <FontAwesome5
+                name="female"
+                size={24}
+                color={`${female ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Female</Text>
             </View>
           </View>
           <View className="mt-5">
-            <TouchableOpacity className="border border-slate-400 p-5 w-[84] h-[84] justify-center rounded-full items-center">
-              <Entypo name="awareness-ribbon" size={24} color="black" />
+            <TouchableOpacity
+              onPress={unisexfunction}
+              className={`border ${
+                unisex ? "border-slate-400" : "bg-[#075ADE]"
+              } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
+            >
+              <Entypo
+                name="awareness-ribbon"
+                size={24}
+                color={`${unisex ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Unisex</Text>
             </View>
           </View>
         </View>
-      </View> */}
+      </View>
 
-      {/* <View className="mb-5">
+      <View className="mb-5">
         <Text className="text-2xl">Your Occupation</Text>
         <View className="flex flex-row justify-evenly mb-5">
           <View className="mt-5">
-            <TouchableOpacity className="border border-slate-400 p-5 w-[84] h-[84] justify-center rounded-full items-center">
-              <Ionicons name="ios-person-outline" size={24} color="black" />
+            <TouchableOpacity
+              onPress={studentfuction}
+              className={`border ${
+                student ? "border-slate-400" : "bg-[#075ADE]"
+              } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
+            >
+              <Ionicons
+                name="ios-person-outline"
+                size={24}
+                color={`${student ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Student</Text>
             </View>
           </View>
           <View className="mt-5">
-            <TouchableOpacity className="border border-slate-400 p-5 w-[84] h-[84] justify-center rounded-full items-center">
-              <Ionicons name="ios-git-network" size={24} color="black" />
+            <TouchableOpacity
+              onPress={occupantfunction}
+              className={`border ${
+                occupant ? "border-slate-400" : "bg-[#075ADE]"
+              } p-5 w-[84] h-[84] justify-center rounded-full items-center`}
+            >
+              <Ionicons
+                name="ios-git-network"
+                size={24}
+                color={`${occupant ? "black" : "white"}`}
+              />
             </TouchableOpacity>
             <View className="mt-2">
               <Text className="text-center text-lg">Worker</Text>
             </View>
           </View>
         </View>
-      </View> */}
+      </View>
 
-      <View>
+      <View className="flex flex-row justify-center">
         <TouchableOpacity
           onPress={createAccount}
-          className="items-center mt-2 bg-[#075ADE] p-5 rounded-[15px]"
+          className="items-center mt-2 bg-[#075ADE] p-5 rounded-full rounded-full]"
         >
           <Text className="text-white font-bold text-[15px]">Get Started</Text>
         </TouchableOpacity>
