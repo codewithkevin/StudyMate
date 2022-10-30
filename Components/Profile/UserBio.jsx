50809533ca28a217dcf487e612074cf665ae8d57
codewithkevin <oseikelvin709@gmail.com>
2022-10-30 14:59:47 +0000

    Working On User Profile

--- Components/Profile/UserBio.jsx
+++ Components/Profile/UserBio.jsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 const UserBio = () => {
   return (
-    <View>
-      <Text>UserBio</Text>
+    <View className="w-[90%]">
+      <View className="mb-4">
+        <Text className="font-medium text-xl">UserName</Text>
+      </View>
+      <View className='mb-5'>
+        <Text>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repudiandae
+          non id, voluptatem accusantium ipsa saepe sed nihil explicabo eaque
+          mollitia praesentium illum, et beatae perferendis voluptatum sint?
+          Ipsam, similique beatae!
+        </Text>
+      </View>
+      <View>
+        <Text>Location</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default UserBio
+export default UserBio;
--- Screens/ProfileScreen.jsx
+++ Screens/ProfileScreen.jsx
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import React from "react";
 import Header from "./../Components/Profile/Header";
+import UserBio from "./../Components/Profile/UserBio";
+import UserInterest from "./../Components/Profile/UserInterest";
 
 const ProfileScreen = () => {
   return (
-    <View className="h-full w-full">
-      <Header />
+    <View className="h-full w-full mb-20">
+      <ScrollView>
+        <Header />
+        <View className="mt-[130] ml-8 w-full">
+          <UserBio />
+          <UserInterest />
+        </View>
+      </ScrollView>
     </View>
   );
 };
