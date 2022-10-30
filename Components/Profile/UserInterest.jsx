2f65f1c0be73db7b89e57f93cd6179e311fdac4b
codewithkevin <oseikelvin709@gmail.com>
2022-10-30 15:01:00 +0000

    Update

--- Components/Profile/UserInterest.jsx
+++ Components/Profile/UserInterest.jsx
@@ -8,17 +8,17 @@ const UserInterest = () => {
       <View className="flex flex-row mt-3 space-x-5">
         <View className="rounded-lg border p-3">
           <TouchableOpacity className="items-center">
-            <Text className="font-normal ">edit profile</Text>
+            <Text className="font-normal ">Crypto</Text>
           </TouchableOpacity>
         </View>
         <View className="rounded-lg border p-3">
           <TouchableOpacity className="items-center">
-            <Text className="font-normal ">edit profile</Text>
+            <Text className="font-normal ">Music</Text>
           </TouchableOpacity>
         </View>
         <View className="rounded-lg border p-3">
           <TouchableOpacity className="items-center">
-            <Text className="font-normal ">edit profile</Text>
+            <Text className="font-normal ">Sports</Text>
           </TouchableOpacity>
         </View>
       </View>
