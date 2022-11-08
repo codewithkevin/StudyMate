9a594ea5f0558df1b8f34bf4266558cd90de4acf
codewithkevin <oseikelvin709@gmail.com>
2022-11-08 01:38:18 +0000

    Update

--- Screens/ProfileSetup/UserInterest.jsx
+++ Screens/ProfileSetup/UserInterest.jsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
 
 const UserInterest = () => {
   return (
-    <View className="">
-      <Text>UserInterst</Text>
-    </View>
-  )
-}
+    <View className="p-4 mt-14">
+      <View className="mb-3">
+        <Image
+          className="w-[150] h-[125px]"
+          source={require("../AuthScreen/Assest/sammy-no-connection.png")}
+        />
+      </View>
 
-export default UserInterest
+      <View className="mb-10">
+        <Text className="text-xl font-normal font-mono ml-5">
+          Select your interests
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+export default UserInterest;
