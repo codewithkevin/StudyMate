2f3b3f9cf759ed40ee1080e42a7443f18cb59ced
codewithkevin <oseikelvin709@gmail.com>
2022-11-10 09:46:58 +0000

    Created Error Handling For Interest Screen

--- Context/ProfileContext/InterestContext.jsx
+++ Context/ProfileContext/InterestContext.jsx
@@ -9,6 +9,13 @@ const InterestContextProvider = (props) => {
   const [game, setGame] = useState(true);
   const [crypto, setCrypto] = useState(true);
   const [sport, setSport] = useState(true);
+  const [cooking, setCooking] = useState(true);
+  const [tech, setTech] = useState(true);
+  const [music, setMusic] = useState(true);
+  const [web, setWeb] = useState(true);
+  const [art, setArt] = useState(true);
+  const [religion, setReligion] = useState(true);
+  const [travelling, setTravelling] = useState(true);
 
   function gamefun(event) {
     event.preventDefault();
@@ -40,6 +47,96 @@ const InterestContextProvider = (props) => {
     }
   }
 
+  const techfun = (event) => {
+    event.preventDefault();
+    if (tech == true) {
+      setTech((current) => !current);
+      setInterest((current) => [...current, "tech"]);
+    } else {
+      setTech((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "tech";
+        })
+      );
+    }
+  };
+
+  const musicfun = (event) => {
+    event.preventDefault();
+    if (music == true) {
+      setMusic((current) => !current);
+      setInterest((current) => [...current, "music"]);
+    } else {
+      setMusic((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "music";
+        })
+      );
+    }
+  };
+
+  const webfun = (event) => {
+    e.preventdefault();
+    if (web == true) {
+      setWeb((current) => !current);
+      setInterest((current) => [...current, "web"]);
+    } else {
+      setWeb((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "web";
+        })
+      );
+    }
+  };
+
+  const artfun = (event) => {
+    event.preventDefault();
+    if (art == true) {
+      setArt((current) => !current);
+      setInterest((current) => [...current, "art"]);
+    } else {
+      setArt((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "art";
+        })
+      );
+    }
+  };
+
+  const religionfun = (event) => {
+    event.preventDefault();
+    if (religion == true) {
+      setReligion((current) => !current);
+      setInterest((current) => [...current, "religion"]);
+    } else {
+      setReligion((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "religion";
+        })
+      );
+    }
+  };
+
+  const travelfun = (event) => {
+    event.preventDefault();
+    if (travelling == true) {
+      setTravelling((current) => !current);
+      setInterest((current) => [...current, "travelling"]);
+    } else {
+      setTravelling((current) => !current);
+      setInterest((current) =>
+        current.filter((element) => {
+          return element !== "travelling";
+        })
+      );
+    }
+  };
+
   function sportfun(event) {
     event.preventDefault();
     if (sport == true) {
@@ -56,6 +153,18 @@ const InterestContextProvider = (props) => {
   }
 
   const values = {
+    religion,
+    travelling,
+    travelfun,
+    religionfun,
+    tech,
+    music,
+    web,
+    art,
+    artfun,
+    webfun,
+    musicfun,
+    techfun,
     game,
     interest,
     gamefun,
